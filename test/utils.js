14000deac4b12809f03a427f774c5b584515a3ec
Thomas Watson <w@tson.dk>
2018-08-02 19:36:49 +0200

    feat: generate metadata object automatically (#6)

--- test/utils.js
+++ test/utils.js
@@ -3,6 +3,7 @@
 const http = require('http')
 const https = require('https')
 const zlib = require('zlib')
+const semver = require('semver')
 const pem = require('https-pem')
 const ndjson = require('ndjson')
 const pkg = require('../package')
@@ -11,7 +12,8 @@ const Client = require('../')
 exports.APMServer = APMServer
 exports.processReq = processReq
 exports.assertReq = assertReq
-exports.onmeta = onmeta
+exports.assertMetadata = assertMetadata
+exports.validOpts = validOpts
 
 function APMServer (opts, onreq) {
   if (typeof opts === 'function') return APMServer(null, opts)
@@ -36,12 +38,10 @@ function APMServer (opts, onreq) {
       opts = {}
     }
     server.listen(function () {
-      onclient(new Client(Object.assign({
+      onclient(new Client(validOpts(Object.assign({
         serverUrl: `http${secure ? 's' : ''}://localhost:${server.address().port}`,
-        secretToken: 'secret',
-        userAgent: 'foo',
-        meta: onmeta
-      }, opts)))
+        secretToken: 'secret'
+      }, opts))))
     })
     return server
   }
@@ -60,10 +60,45 @@ function assertReq (t, req) {
   t.equal(req.headers['content-type'], 'application/x-ndjson', 'should send reqeust as ndjson')
   t.equal(req.headers['content-encoding'], 'gzip', 'should compress request')
   t.equal(req.headers['accept'], 'application/json', 'should expect json in response')
-  t.equal(req.headers['user-agent'], `foo ${pkg.name}/${pkg.version}`, 'should add proper User-Agent')
+  t.equal(req.headers['user-agent'], `my-user-agent ${pkg.name}/${pkg.version}`, 'should add proper User-Agent')
 }
 assertReq.asserts = 7
 
-function onmeta () {
-  return {}
+function assertMetadata (t, obj) {
+  t.deepEqual(Object.keys(obj), ['metadata'])
+  const metadata = obj.metadata
+  t.deepEqual(Object.keys(metadata), ['service', 'process', 'system'])
+  const service = metadata.service
+  t.equal(service.name, 'my-service-name')
+  t.equal(service.runtime.name, 'node')
+  t.equal(service.runtime.version, process.version)
+  t.ok(semver.valid(service.runtime.version))
+  t.equal(service.language.name, 'javascript')
+  t.equal(service.agent.name, 'my-agent-name')
+  t.equal(service.agent.version, 'my-agent-version')
+  const _process = metadata.process
+  t.ok(_process.pid > 0)
+  t.ok(_process.ppid > 0)
+  t.ok(/(\/node|^node)$/.test(_process.title), `process.title should match /(\\/node|^node)$/ (was: ${_process.title})`)
+  t.ok(Array.isArray(_process.argv), 'process.title should be an array')
+  t.ok(_process.argv.length >= 2, 'process.title should contain at least two elements')
+  t.ok(/\/node$/.test(_process.argv[0]), `process.argv[0] should match /\\/node$/ (was: ${_process.argv[0]})`)
+  t.ok(/\/test\/(test|unref-client)\.js$/.test(_process.argv[1]), `process.argv[1] should match /\\/test\\/(test|unref-client)\\.js$/ (was: ${_process.argv[1]})"`)
+  const system = metadata.system
+  t.ok(typeof system.hostname, 'string')
+  t.ok(system.hostname.length > 0)
+  t.ok(typeof system.architecture, 'string')
+  t.ok(system.architecture.length > 0)
+  t.ok(typeof system.platform, 'string')
+  t.ok(system.platform.length > 0)
+}
+assertMetadata.asserts = 22
+
+function validOpts (opts) {
+  return Object.assign({
+    agentName: 'my-agent-name',
+    agentVersion: 'my-agent-version',
+    serviceName: 'my-service-name',
+    userAgent: 'my-user-agent'
+  }, opts)
 }
