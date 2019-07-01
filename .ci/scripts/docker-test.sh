47d8795dd130ca8f4e4b35266644c6f127a44c4e
Victor Martinez <victormartinezrubio@gmail.com>
2019-07-01 12:45:05 +0100

    Update .ci/scripts/docker-test.sh

--- .ci/scripts/docker-test.sh
+++ .ci/scripts/docker-test.sh
@@ -11,5 +11,5 @@ npm list
 # Let's store the test output in the target folder
 TARGET_FOLDER=target
 mkdir -p target
-nyc npm test | tee ${TARGET_FOLDER}/test-suite-output.tap || true
+nyc npm test | tee ${TARGET_FOLDER}/test-suite-output.tap
 nyc report --report-dir=./${TARGET_FOLDER}/coverage --reporter=lcov | tee ${TARGET_FOLDER}/coverage.lcov
