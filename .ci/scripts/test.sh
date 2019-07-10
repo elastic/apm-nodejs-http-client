0f2779e0e9d6a5a14eddcdca702e500d1a06c1ce
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-07-10 15:49:46 +0100

    install nvm if not installed

--- .ci/scripts/test.sh
+++ .ci/scripts/test.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-set -exo pipefail
+set -eo pipefail
 
 NODE_VERSION=${1:?Nodejs version missing NODE_VERSION is not set}
+
+## If the nvm has not been installed yet locally or in the CI then let's install it.
+if ! command -v nvm; then
+  echo 'Installing nvm'
+  curl --silent -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+  source "${HOME}/.nvm/nvm.sh"
+fi
 
 TARGET_FOLDER=target
 
