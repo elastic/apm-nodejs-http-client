9e4823780ea3cf1732570062e612bb318d1ace38
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-07-11 09:55:46 +0100

    remove commentted code and add shellcheck directive for the linted warning

--- .ci/scripts/test.sh
+++ .ci/scripts/test.sh
@@ -7,6 +7,7 @@ NODE_VERSION=${1:?Nodejs version missing NODE_VERSION is not set}
 if ! command -v nvm; then
   echo 'Installing nvm'
   curl --silent -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+  # shellcheck source=/dev/null
   source "${HOME}/.nvm/nvm.sh"
 fi
 
@@ -21,5 +22,3 @@ nvm --version
 npm install
 npm test | tee ${TARGET_FOLDER}/test-suite-output.tap
 npm run coverage
-
-## nyc report --report-dir=./${TARGET_FOLDER}/coverage --reporter=lcov | tee ${TARGET_FOLDER}/coverage.lcov
