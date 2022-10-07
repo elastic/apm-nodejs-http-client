e2b4c65018d2cb9236d9030fbc1aaa72b05276ea
Alan Storm <alan.storm@elastic.co>
2022-10-07 16:40:42 -0700

    fix: linting

--- test/k8s.test.js
+++ test/k8s.test.js
@@ -213,16 +213,6 @@ test('all except kubernetesPodUID', function (t) {
 })
 
 test('Tests for ../lib/container-info', function (t) {
-  const createMockForFixtureString = (source) => {
-    const mock = {
-      sync: () => {
-        const string = source
-        return getContainerInfo.parse(string)
-      }
-    }
-    return mock
-  }
-
   const fixtures = [
     {
       source: '12:freezer:/kubepods.slice/kubepods-pod22949dce_fd8b_11ea_8ede_98f2b32c645c.slice/docker-b15a5bdedd2e7645c3be271364324321b908314e4c77857bbfd32a041148c07f.scope',
